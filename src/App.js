e7c6b42dc3de85d776d4ab0edec5f58e0d994914
Peter Tran <petertranminh21@gmail.com>
2023-01-30 03:48:28 -0500

    spotify api connected

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
 import Login from './Login.js';
+import { getTokenFromUrl } from './Spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+    const [token, setToken] = useState(null);
+
+    useEffect(() => {
+        const hash = getTokenFromUrl();
+        window.location.hash = "";
+        const _token = hash.access_token;
+        
+        if (_token) {
+            setToken(_token);
+            spotify.setAccessToken(_token);
+            spotify.getMe().then((user) => {
+                console.log("USERNAME", user.display_name);
+            });
+            console.log(token);
+        }
+    }, []);
+    
     return (
-        <Login />
+        <div>
+            {!token && <Login />}
+            {token && <div>LOGGED IN</div>}
+        </div>
     );
 }
 export default App;
