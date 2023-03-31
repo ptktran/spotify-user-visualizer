69c9ff8eecbce73c84199f45de7d1546e29731d1
Peter Tran <petertranminh21@gmail.com>
2023-03-31 15:55:19 -0400

    fix

--- src/Login.js
+++ src/Login.js
@@ -3,7 +3,7 @@ import { loginUrl } from './Spotify';
 
 export default function Login() {
     return (
-        <body class="flex h-screen justify-center items-center bg-home-gradient bg-no-repeat bg-black">
+        <body class="flex h-screen justify-center items-center bg-home-gradient bg-no-repeat bg-cover">
             <div class="flex flex-wrap content-center justify-evenly rounded h-96 sm:w-11/12 lg:w-3/6 xl:w-2/6 bg-spotify-grey shadow-gray-900/30 shadow-lg p-10">
                 <div class="basis-full">
                     <h1 class="font-coolvetica text-3xl font-bold tracking-wider basis-full text-center m-3 text-purple-300">BioVisualizer</h1>
